34cd821a4bb0fbef4762b7452b9ec4bafbd7a86b
Mike P. Sinn <m@quantimodo.com>
2024-04-01 16:03:29 -0500

    Set maximum width for chart container and expand public routes
    Maximum width has been set to '200px' in chart-container.tsx to ensure consistent display across devices. Additional public route '/api/warImages' has been added to middleware.ts to accommodate new requirements.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  publicRoutes: ["/", "/api/webhook", "/api/warImages"],
 });
 
 export const config = {
