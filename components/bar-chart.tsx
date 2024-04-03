237c26e65ec7b76f21387f5ff6c13a207c21fe36
Mike P. Sinn <m@crowdsourcingcures.org>
2024-04-03 00:08:48 -0500

    Update BarChart and dashboard page for label positions
    This commit introduces an option to position labels in BarChart either at the top or bottom. Moreover, another BarChart is added on the dashboard page featuring a hard-coded value to display the Average Vote. These modifications offer better flexibility and visual representation of our statistics.

--- components/bar-chart.tsx
+++ components/bar-chart.tsx
@@ -2,11 +2,12 @@
 import React, {useEffect, useState} from 'react';
 
 interface ChartContainerProps {
-  warPercentageDesired: number;
+    warPercentageDesired: number;
+    labelsPosition?: 'top' | 'bottom';
 }
 
 // New BarChart component
-const BarChart: React.FC<ChartContainerProps> = ({ warPercentageDesired }) => {
+const BarChart: React.FC<ChartContainerProps> = ({ warPercentageDesired, labelsPosition = 'top' }) => {
   const researchPercentageDesired = 100 - warPercentageDesired;
   const [warImages, setWarImages] = useState([]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -26,67 +27,83 @@ const BarChart: React.FC<ChartContainerProps> = ({ warPercentageDesired }) => {
     return () => clearInterval(interval);
 }, [warImages.length]);
 
-  return (
+    return (
         <div id="chart-container"
-            style={{display: 'flex', justifyContent: 'center', marginBottom: '5px'}}>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    width: '100%',
-                    maxWidth: '600px',
-                    height: '200px',
-                    justifyContent: 'flex-end'
-                }}>
-                    <div style={{display: 'flex', width: '100%', justifyContent: 'space-between', height: '100%'}}>
-                        <div style={{
-                            width: '48%',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'flex-end',
-                            maxWidth: '200px'
-                        }}>
-                          <span id="warPercentageDesiredLabel"
-                          className="text-sm text-center">
-                            {warPercentageDesired}% War / Military
-                            </span>
-                            <div id="warBar"
-                            style={{
-                                height: `${warPercentageDesired}%`, 
-                                backgroundColor: 'black', 
-                                width: '100%',
-                                backgroundImage: backgroundImage,
-                                backgroundSize: 'cover'
-                            }}>
-                            </div>
-                            
+             style={{display: 'flex', justifyContent: 'center', marginBottom: '5px'}}>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '100%',
+                maxWidth: '600px',
+                height: '200px',
+                justifyContent: 'flex-end'
+            }}>
+                <div style={{display: 'flex', width: '100%', justifyContent: 'space-between', height: '100%'}}>
+                    <div style={{
+                        width: '48%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        maxWidth: '200px'
+                    }}>
+                        {labelsPosition === 'top' && (
+                            <span id="warPercentageDesiredLabel"
+                                  className="text-sm text-center">
+                  {warPercentageDesired}% War / Military
+                </span>
+                        )}
+                        <div id="warBar"
+                             style={{
+                                 height: `${warPercentageDesired}%`,
+                                 backgroundColor: 'black',
+                                 width: '100%',
+                                 backgroundImage: backgroundImage,
+                                 backgroundSize: 'cover'
+                             }}>
                         </div>
-                        <div style={{
-                            width: '48%',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'flex-end',
-                            maxWidth: '200px'
-                        }}>
-                        <span id="researchPercentageDesiredLabel"
-                          className="text-sm text-center px-2">
+                        {labelsPosition === 'bottom' && (
+                            <span id="warPercentageDesiredLabel"
+                                  className="text-sm text-center">
+                  {warPercentageDesired}% War and Military
+                </span>
+                        )}
+                    </div>
+                    <div style={{
+                        width: '48%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        maxWidth: '200px'
+                    }}>
+                        {labelsPosition === 'top' && (
+                            <span id="researchPercentageDesiredLabel"
+                                  className="text-sm text-center px-2">
                             {researchPercentageDesired}% Medical Research
-                        </span>
-                            <div id="researchBar"
-                                style={{
-                                height: `${researchPercentageDesired}%`,
-                                backgroundColor: '#0075ff',
-                                width: '100%',
-                                backgroundImage: 'url(img/people/grandma.jpg)',
-                                backgroundSize: 'cover'
-                            }}></div>
+                            </span>
+                        )}
+                        <div id="researchBar"
+                             style={{
+                                 height: `${researchPercentageDesired}%`,
+                                 backgroundColor: '#0075ff',
+                                 width: '100%',
+                                 backgroundImage: 'url(img/people/grandma.jpg)',
+                                 backgroundSize: 'cover'
+                             }}>
                         </div>
+                        {labelsPosition === 'bottom' && (
+                            <span id="researchPercentageDesiredLabel"
+                                        className="text-sm text-center px-2">
+                            {researchPercentageDesired}% Medical Research
+                            </span>
+                        )}
                     </div>
                 </div>
             </div>
-  );
+        </div>
+    );
 };
 
 export default BarChart;
