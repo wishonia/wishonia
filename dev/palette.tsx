dab71f412b221a117e8b479d6b187bb531a56975
Mike P. Sinn <m@quantimodo.com>
2024-04-17 16:28:49 -0500

    Add dev directory with utility components for React Buddy plugin
    This update includes the addition of a new 'dev' directory. This directory houses utility files and functional React components that enable and describe various visual features for the React Buddy plugin. It includes the configuration for reusable components, component previews, and some global initialization logic for the preview mode.

---
+++ dev/palette.tsx
@@ -0,0 +1,25 @@
+import {Fragment} from "react";
+import {
+    Category,
+    Component,
+    Variant,
+    Palette,
+} from "@react-buddy/ide-toolbox-next";
+
+export const PaletteTree = () => (
+    <Palette>
+        <Category name="App">
+            <Component name="Loader">
+                <Variant>
+                    <ExampleLoaderComponent/>
+                </Variant>
+            </Component>
+        </Category>
+    </Palette>
+);
+
+export function ExampleLoaderComponent() {
+    return (
+        <Fragment>Loading...</Fragment>
+    );
+}
