e5014919e1dc860b59e460434ab8ea1f2a7f15fd
Mike P. Sinn <m@quantimodo.com>
2024-04-17 16:29:58 -0500

    Update website content and layout
    Content and aesthetic changes have been applied across the Hero and Poll pages, with revised text + design changes for better user experience. An English font was also added (IM Fell English) to the global stylesheet.

--- components/pages/hero.tsx
+++ components/pages/hero.tsx
@@ -12,24 +12,21 @@ export default function HeroHeader() {
     <>
       <section className="space-y-8 pb-12 pt-4 md:space-y-16 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-          <p className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium">
-            Global Priorities Referendum on
+          <p className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium font-english">
+            The Magical Kingdom of
           </p>
           <h1 className="text-4xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl">
-            War and Disease
+            Wishonia
           </h1>
-          <p className="max-w-[42rem] text-sm leading-normal text-dark-foreground text-gray-900 font-semibold sm:text-base sm:leading-8">
-            Humanity has a finite amount of brains and resources.
-          </p>
-          <p className="max-w-[42rem] text-sm leading-normal text-dark-foreground text-gray-900 font-semibold sm:text-base sm:leading-8">
-            Adjust how much governments globally should allocate to war/military vs helping 2 billion people suffering from diseases.
+          <p className="max-w-[42rem] text-sm leading-normal font-semibold sm:text-base sm:leading-8">
+            Maximizing universal health and happiness through intelligent voluntary cooperation.
           </p>
           <div className="flex gap-4">
             <Link
                 href="/signin"
                 className={cn(buttonVariants({variant: "default"}))}
             >
-              Get Started
+              Become a Citizen
             </Link>
             <Link
                 href={siteConfig.links.github}
@@ -37,7 +34,7 @@ export default function HeroHeader() {
                 className={cn(buttonVariants({variant: "outline"}))}
             >
               <Icons.github className="mr-2 h-4 w-4"/>
-              <span>GitHub</span>
+              <span>Improve Wishonia</span>
             </Link>
           </div>
         </div>
