915e9cefd6f4fdcd13d34ceb3e53fc795e9fe24e
Mike P. Sinn <m@quantimodo.com>
2024-04-19 12:18:27 -0500

    Refine card titles and comment out unneeded components
    Corrected the title wording of "Total Wishing Well Contributions" in dashboard and stats cards for improved readability. Additionally, commented out unused components such as WishocracyLandingPage, Hero, FeatureCards, and Overview on the frontpage for better readability and performance.

--- components/pages/dashboard/dashboard-cards.tsx
+++ components/pages/dashboard/dashboard-cards.tsx
@@ -55,7 +55,7 @@ export function DashboardCards({ data, searchParams }: DashboardCardsProps) {
       </Card>
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total WishingWellContributions</CardTitle>
+          <CardTitle className="text-sm font-medium">Total Wishing Well Contributions</CardTitle>
           <Icons.history className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
