1954f1bd6fcdccf6803cdde09a3930638f4b0815
Mike P. Sinn <m@quantimodo.com>
2024-05-17 15:17:27 -0500

    Add new pages, components and loading screen for Wishing Wells feature
    Implemented new pages including result page, layout page, and the main Wishing Wells page. Components such as WishingWellsList and PollRandomWishingWells are also added to these pages. Furthermore, a new loading screen with a spinner icon for the Wishing Wells feature has been created.

---
+++ app/wishingWells/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next"
+import { Shell } from "@/components/layout/shell"
+import AfterLoginHandler from "@/components/AfterLoginHandler"
+import {PollRandomWishingWells} from "@/components/poll-random-wishing-wells";
+import {getCurrentUser} from "@/lib/session";
+
+export const metadata: Metadata = {
+  title: "Wishing Wells",
+  description: "Decide how much to allocate to each Wishing Well.",
+}
+
+interface WishingWellProps {
+  searchParams: { thisWishingWellName: string; thatWishingWellName: string }
+}
+
+export default async function WishingWellsPage({ searchParams }: WishingWellProps) {
+    const user = await getCurrentUser()
+
+  return (
+    <Shell>
+        <AfterLoginHandler></AfterLoginHandler>
+       <PollRandomWishingWells user={user}></PollRandomWishingWells>
+    </Shell>
+  )
+}
