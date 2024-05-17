98e3ddf551a3ad2fd655770ad368aa4fda7c851e
Mike P. Sinn <m@quantimodo.com>
2024-05-17 15:14:00 -0500

    Refactor authentication flow and enhance vote saving functionality
    This commit contains significant changes to the authentication flow with removal of 'getServerSession' in favor of 'getUserId' function. This refactoring effort simplifies the codebase and could potentially improve efficiency.
    Additionally, the vote saving functionality has been expanded to incorporate 'wishingWellPairAllocation' and 'globalProblemPairAllocation' in POST requests. Error handling has also been improved across the changes.
    Finally, it adds new endpoints to fetch global problems.

--- app/api/wishingWellPairAllocation/random/route.ts
+++ app/api/wishingWellPairAllocation/random/route.ts
@@ -1,18 +1,17 @@
 import {getUserId} from "@/lib/api/getUserId";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, WishingWell } from '@prisma/client';
+import {convertKeysToCamelCase} from "@/lib/stringHelpers";
 const prisma = new PrismaClient();
 export async function GET() {
     try {
         const userId = await getUserId();
-
-        let randomPair;
-
+        let randomPair: WishingWell[] = [];
         if (userId) {
             randomPair = await prisma.$queryRaw`
           SELECT *
           FROM "wishing_wells"
           WHERE id NOT IN (
-            SELECT "this_wishing_well_id" FROM "wishing_well_pair_allocations" WHERE wishing_well_pair_allocations."user_id" = ${userId}
+            SELECT "this_wishing_well_id" FROM "wishing_well_pair_allocations" WHERE "user_id" = ${userId}
             UNION
             SELECT "that_wishing_well_id" FROM "wishing_well_pair_allocations" WHERE "user_id" = ${userId}
           )
@@ -27,8 +26,13 @@ export async function GET() {
           LIMIT 2;
         `;
         }
+        // Use the helper function to convert keys to camelCase
+        randomPair = randomPair.map(convertKeysToCamelCase);
 
-        return new Response(JSON.stringify(randomPair))
+        return new Response(JSON.stringify({
+          thisWishingWell: randomPair[0],
+          thatWishingWell: randomPair[1],
+        }))
     } catch (error) {
         return new Response(null, { status: 500 })
     }
