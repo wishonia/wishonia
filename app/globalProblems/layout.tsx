4018ec895b965715543905291c3332eb8b95eaea
Mike P. Sinn <m@quantimodo.com>
2024-05-17 15:15:23 -0500

    Add new pages and components for Global Problems feature
    This commit introduces new pages and components required for the Global Problems feature. Specifically, it adds the Global Problems layout, results page, main page, and a loading component. Each of these new additions handles different aspects of representing and managing the Global Problems feature on the application.

---
+++ app/globalProblems/layout.tsx
@@ -0,0 +1,37 @@
+import {generalSidebarNav } from "@/config/links"
+import { getCurrentUser } from "@/lib/session"
+import Footer from "@/components/layout/footer"
+import TopNavbar from "@/components/layout/topNavbar"
+import { SidebarNav } from "@/components/sidebar-nav"
+
+interface DashboardLayoutProps {
+  children: React.ReactNode
+}
+
+export default async function DashboardLayout({
+  children,
+}: DashboardLayoutProps) {
+  const user = await getCurrentUser()
+
+  return (
+    <div className="flex min-h-screen flex-col space-y-6">
+      <TopNavbar
+        user={{
+          name: user?.name,
+          image: user?.image,
+          email: user?.email,
+        }}
+      />
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden w-[200px] flex-col md:flex">
+          <SidebarNav items={generalSidebarNav.data} />
+        </aside>
+        <main className="flex w-full flex-1 flex-col"
+              style={{ maxWidth: "90%" }}>
+            {children}
+        </main>
+      </div>
+      <Footer />
+    </div>
+  )
+}
---
+++ app/globalProblems/results/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next"
+
+import { Shell } from "@/components/layout/shell"
+import { DashboardHeader } from "@/components/pages/dashboard/dashboard-header"
+import {GlobalProblemsList} from "@/components/global-problems-list";
+
+interface GlobalProblemsProps {}
+let heading = `Average Allocations to Each Global Problem`;
+let metaDescription = `Here's the percentage of societal resources the average person would like to contribute to solving each global problem.`;
+export async function generateMetadata({}: GlobalProblemsProps): Promise<Metadata> {
+  return {
+    title: heading,
+    description: metaDescription,
+  }
+}
+
+export default async function GlobalProblems({}: GlobalProblemsProps) {
+  return (
+    <Shell>
+      <DashboardHeader
+        heading={heading}
+        text={metaDescription}
+      >
+      </DashboardHeader>
+      <GlobalProblemsList/>
+    </Shell>
+  )
+}
