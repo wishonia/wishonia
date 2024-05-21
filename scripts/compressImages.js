cf24f0780577ee84d2ebd30b8243c733e6a94499
Mike P. Sinn <m@quantimodo.com>
2024-05-21 15:17:20 -0500

    Add image compression configuration options
    The compressionImages.js script has been updated to provide greater flexibility with image compression configuration. It now allows setting the output quality and the output format for the images. Furthermore, the logic for converting PNG images to JPG has also been added to enable the alteration of the image file format post-compression.

--- scripts/compressImages.js
+++ scripts/compressImages.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const outputQuality = 10; // Set the output quality (0 to 100)
+const outputFormat = 'jpg'; // Set to null to keep the original format
 
 // Function to check if a file is an image
 function isImage(file) {
@@ -26,7 +28,7 @@ function compressImage(imagePath) {
     imagePath = path.resolve(imagePath);
     // create a new directory for compressed images
     fs.mkdirSync(path.join(path.dirname(imagePath), 'compressed'), { recursive: true });
-    const output = path.join(path.dirname(imagePath),
+    let outputFilePath = path.join(path.dirname(imagePath),
         'compressed',
         path.basename(imagePath));
     const ext = path.extname(imagePath).toLowerCase();
@@ -35,19 +37,27 @@ function compressImage(imagePath) {
     switch (ext) {
         case '.jpg':
         case '.jpeg':
-            sharpInstance = sharp(imagePath).jpeg({ quality: 50 });
+            sharpInstance = sharp(imagePath).jpeg({ quality: outputQuality });
             break;
         case '.png':
-            sharpInstance = sharp(imagePath).png({ quality: 10 });
+            if(outputFormat === 'jpg') {
+                sharpInstance = sharp(imagePath)
+                    .jpeg({ quality: outputQuality });
+                outputFilePath = outputFilePath
+                    .replace('.png', '.jpg');
+            } else {
+                sharpInstance = sharp(imagePath)
+                    .png({ quality: outputQuality });
+            }
             break;
         default:
             console.log(`Unsupported image format: ${ext}`);
             return;
     }
 
-    sharpInstance.toFile(output, (err, info) => {
+    sharpInstance.toFile(outputFilePath, (err, info) => {
         if (err) console.log(err);
-        else console.log(`Image compressed: ${output}`);
+        else console.log(`Image compressed: ${outputFilePath}`);
     });
 }
 
