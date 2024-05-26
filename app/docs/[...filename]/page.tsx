11f4f96398f46d574e7025a7c629d89660c25b39
Mike P. Sinn <m@quantimodo.com>
2024-05-25 20:58:34 -0500

    Various pages pertaining to the functionality and components of Wishocracy

--- app/docs/[...filename]/page.tsx
+++ app/docs/[...filename]/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next"
 import MarkdownFileRenderer from "@/components/MarkdownFileRenderer";
 
 export const metadata: Metadata = {
-  title: "Cost of War",
-  description: "Detailed economic analysis of the cost of war.",
+  title: "Docs",
+  description: "Info about the project",
 }
 interface DocsProps {
   params: { filename: string }
