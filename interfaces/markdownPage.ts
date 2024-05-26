a3be42fc619f7ec758027a576105e563a8f1019e
Mike P. Sinn <m@quantimodo.com>
2024-05-25 20:11:18 -0500

    Markdown file list and handle subfolders when serving content

--- interfaces/markdownPage.ts
+++ interfaces/markdownPage.ts
@@ -1,4 +1,15 @@
-import { MarkdownFile } from "./markdownFile";
-export type MarkdownPage = MarkdownFile & {
+import type {Author} from "@/interfaces/author";
+export type MarkdownPage = {
+  slug?: string;
+  name: string;
+  date?: string;
+  featuredImage?: string;
+  author?: Author;
+  description?: string;
+  ogImage?: {
+    url: string;
+  };
+  content: string;
+  preview?: boolean;
   url: string;
 };
