5edeac750fd62691ef1bc225867c4d449851443c
Mike P. Sinn <m@quantimodo.com>
2024-06-04 15:04:41 -0500

    Supabase-js, common tags, mdast-util-from-markdown, openai-edge, and others.

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -21,19 +21,19 @@ Object.defineProperties(globalThis, {
     TransformStream: { value: TransformStream },
     ReadableStream: { value: ReadableStream },
 });
-
-const { Blob, File } = require('node:buffer')
-const { fetch, Headers, FormData, Request, Response } = require('undici')
-
-Object.defineProperties(globalThis, {
-    fetch: { value: fetch, writable: true },
-    Blob: { value: Blob },
-    File: { value: File },
-    Headers: { value: Headers },
-    FormData: { value: FormData },
-    Request: { value: Request },
-    Response: { value: Response },
-})
+// Break API requests during tests
+//const { Blob, File } = require('node:buffer')
+// const { fetch, Headers, FormData, Request, Response } = require('undici')
+//
+// Object.defineProperties(globalThis, {
+//     fetch: { value: fetch, writable: true },
+//     Blob: { value: Blob },
+//     File: { value: File },
+//     Headers: { value: Headers },
+//     FormData: { value: FormData },
+//     Request: { value: Request },
+//     Response: { value: Response },
+// })
 
 
 
