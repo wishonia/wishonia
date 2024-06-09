e6a17af037cdd5f5f849d101c651548b62f0a209
Mike P. Sinn <m@quantimodo.com>
2024-06-09 13:36:00 -0500

    Refactor database dump and load functions
    The functions for dumping and loading the database have been refactored for better modularity and functionality. A new function `dumpTableToJson` was created which is used by `dumpDatabaseToJson`. The `loadJsonToDatabase` function now accepts an optional filter to specify which JSON files to import (based on their name).

--- prisma/seedUser.ts
+++ prisma/seedUser.ts
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/db";
 export async function seedUser() {
     return prisma.user.create({
         data: {
+            id: "clx1cwfzb0000q5t1f68xks0s",
             email: "test@example.com",
             username: "test-user",
             firstName: "Test",
