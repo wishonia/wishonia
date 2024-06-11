553eb28b3ec2b8dc317814f343fa1e7f4f1ba048
Mike P. Sinn <m@quantimodo.com>
2024-06-11 17:45:17 -0500

    Add polling functionality for GlobalProblemSolutions
    Introduced PollSpecificGlobalProblemSolutions and PollRandomGlobalProblemSolutions for specific and random polling of problem solutions. Extended the existing interfaces to handle additional GlobalProblemSolution type. Implemented a new list component to view all problem solutions associated with a particular global problem.

---
+++ components/poll-random-global-problem-solutions.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import {PollSpecificGlobalProblemSolutions} from "@/components/poll-specific-global-problem-solutions";
+import {User} from "next-auth";
+import {SpinningLoader} from "@/components/spinningLoader";
+import {useRouter} from "next/navigation";
+
+interface PollProps {
+    user?: User;
+    globalProblemId: string;
+}
+
+export const PollRandomGlobalProblemSolutions: React.FC<PollProps> = ({ globalProblemId, user }) => {
+    const [globalProblemSolutions, setGlobalProblemSolutions] =
+        useState<{ thisGlobalProblemSolution?: any; thatGlobalProblemSolution?: any }>({});
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+    const fetchGlobalProblemSolutions = async () => {
+        setLoading(true);
+        const response = await fetch('/api/globalProblems/' + globalProblemId + '/solutions/random');
+        debugger
+        const data = await response.json();
+        if(!data.thisGlobalProblemSolution){
+            router.push('/globalProblemSolutions/results');
+            return;
+        }
+        if(!data.thatGlobalProblemSolution){
+            router.push('/globalProblemSolutions/results');
+            return;
+        }
+        setGlobalProblemSolutions(data);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchGlobalProblemSolutions();
+    }, []);
+
+    if (loading) {
+        return <SpinningLoader />;
+    }
+
+    return (
+        <PollSpecificGlobalProblemSolutions thisGlobalProblemSolution={globalProblemSolutions.thisGlobalProblemSolution}
+                            thatGlobalProblemSolution={globalProblemSolutions.thatGlobalProblemSolution}
+                            updatePair={fetchGlobalProblemSolutions}
+                            user={user} />
+    );
+};
