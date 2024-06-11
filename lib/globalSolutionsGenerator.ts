6e51566218a086eab4251d80d527e7c9729af28d
Mike P. Sinn <m@quantimodo.com>
2024-06-10 19:04:28 -0500

    Refactored global solutions image handling, added solution description update feature
    A significant update was made to the `globalSolutionsGenerator.ts`, separating the image saving process into `saveGlobalSolutionImage()` function while maintaining the existing functionality. Additionally, a generalized global solution description generation feature named `generalizeGlobalSolutionDescriptions()` was added which updates global solution descriptions via an API call and stores updated ones to avoid duplication.

--- lib/globalSolutionsGenerator.ts
+++ lib/globalSolutionsGenerator.ts
@@ -5,8 +5,9 @@ import { prisma } from "@/lib/db";
 import {GlobalProblem, GlobalSolution} from "@prisma/client";
 import {generateAndSaveEmbedding} from "@/lib/openai";
 import {saveMarkdownPost} from "@/lib/markdownGenerator";
-import {absPathFromPublic} from "@/lib/fileHelper";
+import {absPathFromPublic, absPathFromRepo} from "@/lib/fileHelper";
 import {linkGlobalProblemSolution} from "@/lib/globalProblemSolutionGenerator";
+import fs from "fs";
 
 const generateImages = false;
 export async function generateGlobalSolutions() {
@@ -180,17 +181,92 @@ export async function generateGlobalSolutionImages() {
             const imageUrl = await generateAndUploadFeaturedImageJpg(
                 `${globalSolution.name} : ${globalSolution.description}`,
                 imagePath);
-            await prisma.globalSolution.update({
-                where: {
-                    id: globalSolution.id
-                },
-                data: {
-                    featuredImage: imageUrl
-                }
-            });
+            await saveGlobalSolutionImage(globalSolution, imageUrl);
         } catch (error) {
             console.error(`Error generating image for global solution: ${globalSolution.name}`);
             console.error(error);
+            try {
+                const imageUrl = await generateAndUploadFeaturedImageJpg(
+                    `${globalSolution.name}`,
+                    imagePath);
+                await saveGlobalSolutionImage(globalSolution, imageUrl);
+            } catch (error) {
+                console.error(`Error generating image for global solution: ${globalSolution.name}`);
+                console.error(error);
+            }
         }
+    }
+}
+
+async function saveGlobalSolutionImage(globalSolution: GlobalSolution, imageUrl: string) {
+    return prisma.globalSolution.update({
+        where: {
+            id: globalSolution.id
+        },
+        data: {
+            featuredImage: imageUrl
+        }
+    });
+}
+
+export async function generalizeGlobalSolutionDescriptions(){
+    const globalSolutions = await prisma.globalSolution.findMany();
+    const globalSolutionsToUpdate = [];
+    for (const globalSolution of globalSolutions) {
+        if(!alreadyUpdated(globalSolution)) {
+            globalSolutionsToUpdate.push(globalSolution);
+        }
+    }
+    let counter = 0;
+    for (const globalSolution of globalSolutionsToUpdate) {
+        counter++;
+        // Log percent complete
+        const percentCompleted = ((counter / globalSolutionsToUpdate.length) * 100).toFixed(2);
+        console.log(`Completed: ${counter}/${globalSolutionsToUpdate.length} (${percentCompleted}%)`);
+        let updatedDescription = await textCompletion(
+            `Create a one sentence description of the global solution "${globalSolution.name}" 
+            for an article about how it could be used to solve global problems.`,
+            'text'
+        );
+        // Remove quote marks
+        updatedDescription = updatedDescription.replace(/"/g, '');
+        updatedDescription = updatedDescription.replace('delves into', 'explores');
+        console.log(`Updated description for ${globalSolution.name}: to 
+        ${updatedDescription} 
+        instead of
+        ${globalSolution.description}`);
+        await prisma.globalSolution.update({
+            where: {
+                id: globalSolution.id
+            },
+            data: {
+                description: updatedDescription
+            }
+        });
+        cacheUpdatedId(globalSolution);
+    }
+}
+
+function cacheUpdatedId(globalSolution: GlobalSolution) {
+    const filePath = absPathFromRepo('updatedGlobalSolutionIds.json');
+    // Cache the updated id's to a file so we can avoid duplication
+    if(!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, JSON.stringify([globalSolution.id]));
+        return;
+    }
+    const str = fs.readFileSync(filePath);
+    const updatedIds = JSON.parse(str.toString());
+    updatedIds.push(globalSolution.id);
+    fs.writeFileSync(filePath, JSON.stringify(updatedIds));
+}
+
+function alreadyUpdated(globalSolution: GlobalSolution) {
+    const filePath = absPathFromRepo('updatedGlobalSolutionIds.json');
+    try {
+        const str = fs.readFileSync(filePath);
+        const updatedIds = JSON.parse(str.toString());
+        return updatedIds.includes(globalSolution.id);
+    } catch (error) {
+        return false;
     }
 }
