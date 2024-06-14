104089b362992c47942fadc2a734d8d3ca133533
Mike P. Sinn <m@quantimodo.com>
2024-06-14 14:20:57 -0500

    Add globalSolutionNames and update userId in JSON files
    Added a new globalSolutionNames file for aging solutions. Also, updated the userId in GlobalProblemPairAllocation.json and GlobalProblemSolutionPairAllocation.json files. Now, all entries are associated with a generic "test-user" for consistency in testing.

---
+++ prisma/seeds/globalSolutionNames.ts
@@ -0,0 +1,25 @@
+export const globalSolutionNames = {
+    aging:  [
+        "Develop Senolytics Drugs",
+        "Optimize Rapamycin Dosage",
+        "Conduct Clinical Trials on Resveratrol",
+        "Create Personalized Longevity Diet Plans",
+        "Identify Pro-Longevity Gene Variants",
+        "Improve Cellular Reprogramming Efficiency",
+        "Map Epigenetic Markers of Aging",
+        "Develop Targeted Stem Cell Delivery Methods",
+        "Engineer Mitochondria-Rejuvenating Nanoparticles",
+        "Bio-Print Replacement Organs",
+        "Isolate and Characterize Longevity-Promoting Biomolecules",
+        "Develop AI Algorithms for Personalized Longevity Treatments",
+        "Synthesize and Test Longevity-Enhancing Peptides",
+        "Develop Mitochondrial Transplantation Techniques",
+        "Optimize Hormone Replacement Therapy Protocols",
+        "Conduct Longitudinal Studies on Longevity Lifestyles",
+        "Investigate Cryotherapy for Longevity",
+        "Develop Biocompatible Artificial Organs",
+        "Create High-Fidelity Brain-Computer Interfaces",
+        "Study the Longevity Effects of Young Blood Plasma",
+        "Identify Longevity-Promoting Gut Bacteria"
+    ]
+};
