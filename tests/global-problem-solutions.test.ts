f9182544fc8c8d65f99242f4f2b54a7fd7581c44
Mike P. Sinn <m@quantimodo.com>
2024-06-14 14:19:12 -0500

    Refactor global problem solutions tests and separate dump test
    This commit refactors the 'global-problem-solutions' test by simplifying the test cases and removing unnecessary cleaning and loading functions. The database dumping test case has been moved into a separate 'dump.test.ts' file. In addition, some cleanup was done in the 'seed.test.ts' file by eliminating an unnecessary call to dump the database to JSON files.

--- tests/global-problem-solutions.test.ts
+++ tests/global-problem-solutions.test.ts
@@ -2,61 +2,21 @@
  * @jest-environment node
  */
 import {PrismaClient} from "@prisma/client";
-import {getOrCreateTestUser} from "@/tests/test-helpers";
-import {loadJsonToDatabase} from "@/lib/prisma/dumpDatabaseToJson";
-import {generateGlobalProblemSolutionsForGlobalProblem} from "@/lib/globalSolutionsGenerator";
-import {absPathFromRepo} from "@/lib/fileHelper";
-import fs from "fs";
+import {createGlobalProblemSolution} from "@/lib/globalProblemSolutionGenerator";
+import {globalSolutionNames} from "@/prisma/seeds/globalSolutionNames";
 let prisma = new PrismaClient();
 
-function cleanArray(arr: Array<any>): Array<any> {
-    // remove objects without label or empty label
-    arr = arr.filter(obj => obj.label && obj.label !== '');
-    return arr.map(obj => {
-        const original = JSON.parse(JSON.stringify(obj));
-        delete obj.nodelabelfontsize;
-        delete obj.fill;
-        delete obj.pos;
-        delete obj.kind;
-        delete obj.id;
-        for (let key in obj) {
-            let value = obj[key];
-            if(typeof value === 'string') {
-                value = value.trim();
-            }
-            if (typeof value === 'boolean' || value === '' || !value) {
-                delete obj[key];
-            }
-            // delete empty arrays
-            if (Array.isArray(obj[key]) && obj[key].length === 0) {
-                delete obj[key];
-            }
-        }
-        return obj;
-    });
-}
 describe("Global Problem Solutions", () => {
     jest.setTimeout(6000000);
-    it("Cleans the longevity tech tree", async () => {
-        const str = fs.readFileSync(absPathFromRepo("public/data/longevity-tech-tree.json"), 'utf8');
-        const arr = JSON.parse(str);
-        const cleaned = cleanArray(arr.items);
-        fs.writeFileSync(absPathFromRepo("public/data/longevity-tech-tree-cleaned.json"), JSON.stringify(cleaned, null, 4));
-    });
-    it("Generates GlobalProblemSolutions for aging", async () => {
-        const testUser = await getOrCreateTestUser();
-        const globalProblem  = await prisma.globalProblem.findFirst({
+    it("Creates Global Problem Solutions for Aging", async () => {
+        const globalProblem = await prisma.globalProblem.findFirst({
             where: {
                 name: "Aging"
             }
         });
-        if(!globalProblem) throw new Error("Global Problem Aging not found");
-        const globalProblemSolutions =
-            await generateGlobalProblemSolutionsForGlobalProblem(globalProblem);
-
+        if (!globalProblem) throw new Error("Global Problem Aging not found");
+        for (const title of globalSolutionNames.aging) {
+            await createGlobalProblemSolution(title, null, globalProblem);
+        }
     });
-    it("Load GlobalProblemSolution fixture", async () => {
-        await loadJsonToDatabase("GlobalProblemSolution");
-    }, 6000000);
-
 });
