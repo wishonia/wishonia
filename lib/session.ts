b4f4018a57e4a6c837ce5b29de20279214bc9593
Mike P. Sinn <m@quantimodo.com>
2024-06-15 14:52:50 -0500

    Add isAdmin function in session library
    A new function called `isAdmin` has been added to the session library. This function checks if the current user is an administrator by calling the `getCurrentUser` function and querying the database using prisma. If the user does not exist or is not an admin, it returns false.

--- lib/session.ts
+++ lib/session.ts
@@ -1,9 +1,22 @@
 import { getServerSession } from "next-auth/next"
 
 import { authOptions } from "@/lib/auth"
+import { prisma } from "@/lib/db"
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions)
 
   return session?.user
+}
+
+export async function isAdmin() {
+  const sessionUser = await getCurrentUser()
+  if(!sessionUser) return false
+  const user = await prisma.user.findUnique({
+    where: {
+      id: sessionUser.id
+    }
+  })
+
+  return user?.admin
 }
