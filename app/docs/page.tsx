8da1e3324ace146c9b26af2ff087dee6595530ff
Mike P. Sinn <m@quantimodo.com>
2024-06-15 14:43:25 -0500

    Docs page

---
+++ app/docs/page.tsx
@@ -0,0 +1,11 @@
+import MarkdownPagesList from "@/components/markdown-list";
+import {Shell} from "@/components/layout/shell";
+export default function MarkdownPageListPage() {
+
+    return (
+        <Shell>
+            <MarkdownPagesList></MarkdownPagesList>
+        </Shell>
+    )
+
+}
