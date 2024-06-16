6961b154e5c0e19a305f049649f97dc52007330b
Mike P. Sinn <m@quantimodo.com>
2024-06-15 20:22:00 -0500

    Update anonymous vote button for user authentication
    Revised the anonymous vote button to reflect current authorization status and implemented a modal user authentication form. Moreover, the user authentication form has been modified to include callback URL and adjust user sign-in methods to accommodate this change. This streamlines the voting process by correctly maintaining the position on the page post-authentication.

--- components/anonymous-vote-button.tsx
+++ components/anonymous-vote-button.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import React from "react";
-import {User} from "next-auth";
+import React, { useState } from "react";
+import { User } from "next-auth";
+import {UserAuthForm} from "@/components/user/user-auth-form";
 
 interface PollProps {
     user?: User;
@@ -11,20 +11,34 @@ export const AnonymousVoteButton: React.FC<PollProps> = ({ user }) => {
     if (user) {
         return null;
     }
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleVoteButtonClick = () => {
+        const currentUrl = window.location.href;
+        localStorage.setItem("returnUrl", currentUrl);
+        setShowModal(true);
+    };
+
     return (
         <div id="not-logged-in-container">
-            <Link id="vote-button" href="/signup">
-                <Button
+            <div id="vote-button" >
+                <Button onClick={handleVoteButtonClick}
                     className="text-xl p-6 md:p-8 rounded-full font-semibold hover:border hover:border-black mt-2"
                 >
                     Vote
                 </Button>
-            </Link>
+            </div>
             <div className="">
                 <div className="text-xs px-4 pt-4">
                     It's necessary to sign in to ensure electoral integrity. Robots don't get to vote!
                 </div>
             </div>
+            {showModal && (
+                <div className="modal">
+                    <UserAuthForm callbackUrl={window.location.href}/>
+                </div>
+            )}
         </div>
     );
 };
