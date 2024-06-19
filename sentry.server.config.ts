b49c59d443e3a0090c7db8a006b122dd4f0d5eb1
Mike P. Sinn <m@quantimodo.com>
2024-06-19 14:02:18 -0500

    Add Sentry integration for error tracking
    This commit adds Sentry configuration for both client, server, and edge environments, enabling detailed error tracking. It also includes a new instrumentation file to decide which Sentry config to use based on the runtime environment. Additionally, Sentry-specific options are added to the next.config.js file and Sentry files are included in the .gitignore file. Relevant changes are made in package.json as well.

---
+++ sentry.server.config.ts
@@ -0,0 +1,25 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5f75ae1f23ea666edacb0fd9e2102016@o926849.ingest.us.sentry.io/4507456052985856",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+   spotlight: process.env.NODE_ENV === 'development',
+
+  // Set profilesSampleRate to 1.0 to profile every transaction.
+  // Since profilesSampleRate is relative to tracesSampleRate,
+  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+  // results in 25% of transactions being profiled (0.5*0.5=0.25)
+  profilesSampleRate: 1.0,
+});
