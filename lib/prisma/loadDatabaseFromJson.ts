c23f248b6b98accb2ac718740bbad1532e5f9327
Mike P. Sinn <m@crowdsourcingcures.org>
2024-06-20 01:22:10 -0500

    Refactor database loading function and update Prisma seed data
    The database loading function in the Prisma library was refactored to optimize the loading process from JSON dump files. The loading process no longer fetches all JSON

--- lib/prisma/loadDatabaseFromJson.ts
+++ lib/prisma/loadDatabaseFromJson.ts
@@ -1,57 +1,33 @@
 import fs from 'fs';
 import {absPathFromRepo} from "@/lib/fileHelper";
 import {getPostgresClient, getSchemaName} from "@/lib/db/postgresClient";
-
-export function readDumpFile(file: string) {
-    if(!file.endsWith('.json')) {
-        file += '.json';
-    }
-    // Read the JSON file
-    const absPath = absPathFromRepo(`prisma/seeds/${file}`);
+export function readDumpFile(tableName: string) {
+    const absPath = absPathFromRepo(`prisma/seeds/${tableName}.json`);
     const jsonData = fs.readFileSync(absPath, 'utf-8');
-
-    // Parse the JSON data
     return JSON.parse(jsonData);
 }
-
-export async function loadJsonToDatabase(filter?: string) {
+export async function loadJsonToDatabase(tableName: string, userId?: string) {
     try {
-        // Get the PostgreSQL client
         const pool = getPostgresClient();
-
-        // Get all JSON files from the prisma/seeds directory
-        const seedsDirectory = absPathFromRepo('prisma/seeds');
-        const jsonFiles = fs.readdirSync(seedsDirectory).filter(file => file.endsWith('.json'));
-
-        // Loop through each JSON file and import its data
-        for (const file of jsonFiles) {
-            if(filter && !file.includes(filter)) {
-                continue;
-            }
-            const tableName = file.replace('.json', '');
-            const data = readDumpFile(file);
-
-            // Get the column names from the first object in the data array
-            const columns = Object.keys(data[0]);
-
-            // Prepare the INSERT statement with the specified schema
-            const schema = getSchemaName();
-            const insertQuery = `
-        INSERT INTO "${schema}"."${tableName}" (${columns.map(column => `"${column}"`).join(', ')})
-        VALUES (${columns.map((_, index) => `$${index + 1}`).join(', ')})
-        ON CONFLICT DO NOTHING
+        const data = readDumpFile(tableName);
+        const columns = Object.keys(data[0]);
+        const schema = getSchemaName();
+        const insertQuery = `
+            INSERT INTO "${schema}"."${tableName}" (${columns.map(column => `"${column}"`).join(', ')})
+            VALUES (${columns.map((_, index) => `$${index + 1}`).join(', ')})
+            ON CONFLICT DO NOTHING
       `;
 
-            // Insert the data into the current table using pg
-            for (const row of data) {
-                const values = columns.map(column => row[column]);
+        for (const row of data) {
+            if(userId)  {row.userId = userId;}
+            const values = columns.map(column => row[column]);
+            try {
                 await pool.query(insertQuery, values);
+            } catch (e) {
+                debugger
+                console.log(`Error importing data for table: ${tableName}`, e);
             }
-
-            console.log(`Data imported for table: ${tableName}`);
         }
-
-        console.log('Data imported successfully.');
     } finally {
         // Get the PostgreSQL client
         const pool = getPostgresClient();
--- prisma/seedGlobalProblemPairAllocations.ts
+++ prisma/seedGlobalProblemPairAllocations.ts
@@ -1,5 +1,5 @@
 import {User} from "@prisma/client";
-import {getAllRandomGlobalProblemPairs} from "@/lib/globalProblems";
+import {aggregateGlobalProblemPairAllocations, getAllRandomGlobalProblemPairs} from "@/lib/globalProblems";
 import { prisma } from "@/lib/db";
 
 export async function seedGlobalProblemPairAllocations(testUser: User) {
@@ -7,15 +7,31 @@ export async function seedGlobalProblemPairAllocations(testUser: User) {
     let idsWithPair: string[] = [];
     for (const pair of pairs) {
         if(idsWithPair.includes(pair[0].id) && idsWithPair.includes(pair[1].id)) {continue;}
-        const result = await prisma.globalProblemPairAllocation.create({
-            data: {
-                thisGlobalProblemId: pair[0].id,
-                thatGlobalProblemId: pair[1].id,
-                userId: testUser.id,
-                thisGlobalProblemPercentage: 50,
-            },
-        });
-        idsWithPair.push(pair[0].id);
-        idsWithPair.push(pair[1].id);
+        try {
+            const existingAllocation = await prisma.globalProblemPairAllocation.findFirst({
+                where: {
+                    thisGlobalProblemId: pair[0].id,
+                    thatGlobalProblemId: pair[1].id,
+                    userId: testUser.id,
+                },
+            });
+            if(existingAllocation) {
+                continue;
+            }
+            const result = await prisma.globalProblemPairAllocation.create({
+                data: {
+                    thisGlobalProblemId: pair[0].id,
+                    thatGlobalProblemId: pair[1].id,
+                    userId: testUser.id,
+                    thisGlobalProblemPercentage: 50,
+                },
+            });
+            idsWithPair.push(pair[0].id);
+            idsWithPair.push(pair[1].id);
+        } catch (e) {
+            debugger
+            console.log(`Error allocating ${pair[0].name} and ${pair[1].name}`);
+        }
     }
+    await aggregateGlobalProblemPairAllocations();
 }
