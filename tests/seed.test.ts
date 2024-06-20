816d9934dd999e2c8815d793b9dab77abf07cca6
Mike P. Sinn <m@crowdsourcingcures.org>
2024-06-20 01:22:10 -0500

    Refactor database loading function and update Prisma seed data
    The database loading function in the Prisma library was refactored to optimize the loading process from JSON dump files. The loading process no longer fetches all JSON

--- tests/seed.test.ts
+++ tests/seed.test.ts
@@ -2,7 +2,9 @@
  * @jest-environment node
  */
 import {loadJsonToDatabase} from "@/lib/prisma/loadDatabaseFromJson";
-import {assertTestDB} from "@/tests/test-helpers";
+import {assertTestDB, getOrCreateTestUser} from "@/tests/test-helpers";
+import {seedGlobalProblemPairAllocations} from "@/prisma/seedGlobalProblemPairAllocations";
+import {seedGlobalProblemSolutionPairAllocations} from "@/prisma/seedGlobalProblemSolutionPairAllocations";
 beforeAll(async () => {
     await assertTestDB();
 });
@@ -10,11 +12,14 @@ beforeAll(async () => {
 describe("Database-seeder tests", () => {
     jest.setTimeout(6000000);
     it("Seeds from JSON dump", async () => {
+        const testUser = await getOrCreateTestUser();
         await loadJsonToDatabase("User");
-        await loadJsonToDatabase("GlobalSolution");
-        await loadJsonToDatabase("GlobalProblem");
-        await loadJsonToDatabase("GlobalProblemSolution");
-        await loadJsonToDatabase("WishingWell");
+        await loadJsonToDatabase("GlobalSolution", testUser.id);
+        await loadJsonToDatabase("GlobalProblem", testUser.id);
+        await loadJsonToDatabase("GlobalProblemSolution", testUser.id);
+        await seedGlobalProblemPairAllocations(testUser);
+        await seedGlobalProblemSolutionPairAllocations(testUser);;
+        await loadJsonToDatabase("WishingWell", testUser.id);
     });
 });
 
