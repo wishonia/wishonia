083971df764a189ce4e6d7ad41800884dc9ffa83
Mike P. Sinn <m@crowdsourcingcures.org>
2024-06-21 18:16:58 -0500

    Add global solution functionalities and methods for creating, editing and deleting global solutions. Additionally, included a function to verify if a user can access a solution. Also, adjusted the labelling in the global solution edit form and added a method to check if the code runs in a command-line interface.

---
+++ app/api/globalSolutions/[globalSolutionId]/route.ts
@@ -0,0 +1,76 @@
+import * as z from "zod"
+
+import { verifyGlobalSolution } from "@/lib/api/globalSolutions"
+import { prisma as db } from "@/lib/db"
+import { globalSolutionPatchSchema } from "@/lib/validations/globalSolution"
+import {handleError} from "@/lib/errorHandler";
+
+const routeContextSchema = z.object({
+    params: z.object({
+        globalSolutionId: z.string(),
+    }),
+})
+
+export async function PATCH(
+    req: Request,
+    context: z.infer<typeof routeContextSchema>
+) {
+    try {
+        const { params } = routeContextSchema.parse(context)
+
+        if (!(await verifyGlobalSolution(params.globalSolutionId))) {
+            return new Response(null, { status: 403 })
+        }
+
+        const json = await req.json()
+        const body = globalSolutionPatchSchema.parse(json)
+
+        // Update the globalSolution
+        await db.globalSolution.update({
+            where: {
+                id: params.globalSolutionId,
+            },
+            data: {
+                name: body.name,
+                description: body.description,
+                updatedAt: new Date(),
+            },
+        })
+
+        return new Response(null, { status: 200 })
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return new Response(JSON.stringify(error.issues), { status: 422 })
+        }
+
+        return handleError(error)
+    }
+}
+
+export async function DELETE(
+    req: Request,
+    context: z.infer<typeof routeContextSchema>
+) {
+    try {
+        const { params } = routeContextSchema.parse(context)
+
+        if (!(await verifyGlobalSolution(params.globalSolutionId))) {
+            return new Response(null, { status: 403 })
+        }
+
+        // Delete the globalSolution
+        await db.globalSolution.delete({
+            where: {
+                id: params.globalSolutionId as string,
+            },
+        })
+
+        return new Response(null, { status: 204 })
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return new Response(JSON.stringify(error.issues), { status: 422 })
+        }
+
+        return handleError(error)
+    }
+}
