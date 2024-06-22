e0e7856893868f08214a1a914c0c9767fc6f1157
Mike P. Sinn <m@crowdsourcingcures.org>
2024-06-22 03:34:25 -0500

    Update email server setup and add mailer tests
    The .env.example file has been updated to use API keys, replacing the previous SMTP server setup. Simultaneously, new files have also been added: a 'mailer.ts' which houses a 'sendEmail' function using nodemailer, and a 'mailer.test.ts' which tests this sendEmail function. These changes help to simplify the process of sending emails and improve the test coverage of the codebase.

---
+++ tests/mailer.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment node
+ */
+import { sendEmail } from '@/lib/mailer';
+describe('sendEmail', () => {
+    it('should send an email with correct parameters', async () => {
+        const mockEmail = 'test@example.com';
+        const mockSubject = 'Test Subject';
+        const mockText = 'Test Text';
+        const mockHtml = '<b>Test Html</b>';
+       const info = await sendEmail(mockEmail, mockSubject, mockText, mockHtml);
+        expect(info.rejected).toStrictEqual([]);
+    });
+});
