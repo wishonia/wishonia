4b9f1ba95dcec23b12dda63e66a065c027f4489a
Mike P. Sinn <m@quantimodo.com>
2024-06-24 00:21:19 -0500

    Disable Sentry and correct Prisma client imports
    The updates to the Sentry initialization now skip the process when the SENTRY_AUTH_TOKEN environment variable isn't set. Doing this can prevent potential crashes during startup in the absence of the token. Also, corrected the import path for Prisma client in multiple files to ensure the correct functioning of Prisma operations.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,8 @@
 export async function register() {
+  if(!process.env.SENTRY_AUTH_TOKEN) {
+    console.debug('SENTRY_AUTH_TOKEN is not set, skipping Sentry initialization');
+    return;
+  }
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config');
   }
