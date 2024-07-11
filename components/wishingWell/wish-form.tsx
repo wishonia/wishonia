5e79a8488ccbbfc4fb2c76915e3c11cda9cf2474
Mike P. Sinn <m@quantimodo.com>
2024-07-10 19:08:33 -0600

    Add unique constraints to schema.prisma
    The 'url' field in the Agent model has been updated to be unique. Additionally, a composite unique constraint has been established on the 'name' and 'userId' fields to ensure that each user has uniquely named agents.

--- components/wishingWell/wish-form.tsx
+++ components/wishingWell/wish-form.tsx
@@ -4,7 +4,6 @@ import * as React from "react"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { WishingWell } from "@prisma/client"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 
