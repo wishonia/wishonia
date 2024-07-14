f189128404252298b6031aababa808f62d96f7e1
Mike P. Sinn <m@quantimodo.com>
2024-07-14 15:47:43 -0600

    deprecated Icons.spinner element with the new SpinningLoader component across the application, including those in the dashboard and other feature sections. This standardizes the loading animations and helps unify the overall user experience.

--- app/voters/loading.tsx
+++ app/voters/loading.tsx
@@ -1,12 +1,10 @@
-import { Icons } from "@/components/icons"
 import { Shell } from "@/components/layout/shell"
+import {SpinningLoader} from "@/components/spinningLoader";
 
 export default async function DashboardLoading() {
   return (
     <Shell>
-      <div className="flex justify-center p-8">
-        <Icons.spinner className="animate-spin text-4xl" />
-      </div>
+        <SpinningLoader />
     </Shell>
   )
 }
