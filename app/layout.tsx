d0245c9fb27b4fe7427882c2a2e778d65560962b
Mike P. Sinn <m@quantimodo.com>
2024-07-16 13:33:22 -0600

    Change default theme to dark
    The default theme of the layout component has been changed from light to dark. This will provide users with a darker user interface by default, which can be easier on the eyes in low-light conditions.

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" suppressHydrationWarning>
       <body className={cn("antialiased", inter.className)}>
         <CopilotKit url="/api/copilot/openai/">
-          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             <NextTopLoader color="#DC2645" height={2.5} showSpinner={false} />
             <div
               vaul-drawer-wrapper=""
