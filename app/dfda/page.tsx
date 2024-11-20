2c62dfc559137012efb5f427fa4faa7c87e15871
Mike P. Sinn <m@crowdsourcingcures.org>
2024-11-20 10:27:50 -0600

    Refactor HomePage component and update related imports
    Replaces HomePage component with DFDAHomePage and updates its structure. Adjusts related imports and incorporates a new VariableSearchAutocomplete with optimized caching.
    
    Took 35 minutes

--- app/dfda/page.tsx
+++ app/dfda/page.tsx
@@ -1,11 +1,6 @@
 "use client"
 import React from 'react'
-import FindTreatments from './components/FindTreatments'
-import HealthTrackingLinkBoxes from './components/HealthTrackingLinkBoxes'
-import CostBenefitAnalysis from './components/CostBenefitAnalysis'
-import TopTreatments from './components/TopTreatments'
-import TreatmentConditionSearchBox from "@/app/dfda/components/TreatmentConditionSearchBox";
-import HomePage from './components/home-page'
+import HomePage from './components/dfda-home-page'
 
 export default function DFDAPage() {
     return (
