88ffb571fae4d7d9d9ee2f11a33942316847f908
Mike P. Sinn <m@crowdsourcingcures.org>
2024-11-21 20:37:45 -0600

    meta stuff
    Took 9 minutes

--- app/dfda/layout.tsx
+++ app/dfda/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react"
+import { Metadata } from "next"
 
+import { domainConfigs } from "@/config/domains"
 import { getNavigationForDomain } from "@/config/navigation"
 import { getCurrentUser } from "@/lib/session"
 
@@ -9,6 +11,54 @@ import DfdaTopNavbar from "./components/DfdaTopNavbar"
 
 interface DFDALayoutProps {
   children: React.ReactNode
+}
+
+const dfdaConfig = domainConfigs["dfda.earth"]
+
+export const metadata: Metadata = {
+  metadataBase: new URL(dfdaConfig.url.base),
+  title: {
+    default: dfdaConfig.name,
+    template: `%s | ${dfdaConfig.name}`,
+  },
+  description: dfdaConfig.description,
+  keywords: dfdaConfig.keywords,
+  authors: [
+    {
+      name: dfdaConfig.author,
+      url: dfdaConfig.url.author,
+    },
+  ],
+  creator: dfdaConfig.author,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: dfdaConfig.url.base,
+    title: dfdaConfig.name,
+    description: dfdaConfig.description,
+    siteName: dfdaConfig.name,
+    images: [
+      {
+        url: dfdaConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: dfdaConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: dfdaConfig.name,
+    description: dfdaConfig.description,
+    images: [dfdaConfig.ogImage],
+    creator: "@thinkbynumbers",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 }
 
 export default async function DFDALayout({ children }: DFDALayoutProps) {
