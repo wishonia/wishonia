de0aaef15138da85b829711c6770b47a4880a75f
Mike P. Sinn <m@crowdsourcingcures.org>
2024-11-24 13:12:26 -0600

    Remove wish layout and loading components; streamline wish form
    Deleted `layout.tsx` and `loading.tsx` in the wish directory to reduce redundancy. Simplified the wish form, added user feedback on submission status, and ensured smoother navigation after form submission.
    
    Took 32 minutes

--- app/wish/page.tsx
+++ app/wish/page.tsx
@@ -1,38 +1,13 @@
 import React from "react"
-import { Metadata } from "next"
-import { redirect } from "next/navigation"
 
-import { authOptions } from "@/lib/auth"
-import { getCurrentUser } from "@/lib/session"
-import { Shell } from "@/components/layout/shell"
-import { DashboardHeader } from "@/components/pages/dashboard/dashboard-header"
 import { WishForm } from "@/components/wishingWell/wish-form"
-import {WishingWellsList} from "@/components/wishing-well-list";
 
-export const metadata: Metadata = {
-  title: "Make a Wish",
-}
-
-interface WishingWellEditProps {
-  params: { wishingWellId?: string }
-}
-
-export default async function WishingWellEdit({
-  params,
-}: WishingWellEditProps) {
-  const user = await getCurrentUser()
-
-  if (!user) {
-    redirect(authOptions?.pages?.signIn || "/signin")
-  }
-
+export default async function WishingWellEdit() {
   return (
-    <Shell>
-      <DashboardHeader heading="Make a Wish" text="Choose wisely!" />
-      <div className="grid grid-cols-1 gap-10">
-        <WishForm></WishForm>
-          <WishingWellsList />
+    <div className="min-h-screen bg-white p-4">
+      <div className="mx-auto flex min-h-screen max-w-xl flex-col justify-center">
+        <WishForm />
       </div>
-    </Shell>
+    </div>
   )
 }
