d8c3ec0fd987bbe98fc1cef6fb8b1543497ab01d
Mike P. Sinn <m@crowdsourcingcures.org>
2024-12-30 18:59:01 -0600

    Removed unused imports

--- prisma/seedWishingWellPairAllocations.ts
+++ prisma/seedWishingWellPairAllocations.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, User } from "@prisma/client"
+import { User } from "@prisma/client"
 
 import { prisma } from "@/lib/db"
 import { createMinimalWishingWellPairs } from "@/lib/wishingWells"
