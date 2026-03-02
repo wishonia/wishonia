9c858d7514ec9c9dd099618470aa2fbe0a2c529d
Mike P. Sinn <m@crowdsourcingcures.org>
2026-03-01 22:35:52 -0600

    Refactor frontpage components and remove unused code
    - Simplified the `FrontPageLayout` by removing the conditional rendering for `DFDALayout`, streamlining the layout structure.
    - Updated the `Home` component to eliminate the host check and direct rendering of `DFDAPage`, enhancing clarity and performance.
    - Cleaned up imports by removing unused references to `headers` and `DFDA` components.

--- app/(frontpage)/page.tsx
+++ app/(frontpage)/page.tsx
@@ -7,16 +7,10 @@ import { PWARedirect } from "@/components/pwa-redirect";
 import InteractiveLandingPage from "@/components/landingPage/interactive-landing-page";
 import WorldOptimizationDashboard from "@/components/landingPage/world-optimization-dashboard";
 import ManualHero from "@/components/landingPage/ManualHero";
-import { headers } from "next/headers"
-import DFDAPage from "../dfda/page"
 
 export const revalidate = 3600
 
-export default async function Home() {
-  const headersList = headers()
-  const host = headersList.get('host') || ''
-  if(host.includes('dfda')) {return <DFDAPage />}
-
+export default function Home() {
   return (
       <main className="mx-auto max-w-7xl">
           <ManualHero/>
