484e8b1f7d385b56c2509b224d086e5cd9a57a46
Mike P. Sinn <m@crowdsourcingcures.org>
2026-03-01 22:35:52 -0600

    Refactor frontpage components and remove unused code
    - Simplified the `FrontPageLayout` by removing the conditional rendering for `DFDALayout`, streamlining the layout structure.
    - Updated the `Home` component to eliminate the host check and direct rendering of `DFDAPage`, enhancing clarity and performance.
    - Cleaned up imports by removing unused references to `headers` and `DFDA` components.

--- app/(frontpage)/layout.tsx
+++ app/(frontpage)/layout.tsx
@@ -2,20 +2,14 @@ import React from "react"
 
 import Footer from "@/components/layout/footer"
 import { ClientTopNavbar } from "@/components/layout/ClientTopNavbar"
-import { headers } from "next/headers"
-import DFDALayout from "../dfda/layout"
 
 interface FrontPageLayoutProps {
   children: React.ReactNode
 }
 
-const FrontPageLayout = async ({
+const FrontPageLayout = ({
   children,
 }: FrontPageLayoutProps) => {
-    const headersList = headers()
-    const host = headersList.get('host') || ''
-    if (host.includes('dfda')) {return <DFDALayout>{children}</DFDALayout>}
-
     return (
       <>
         <ClientTopNavbar />
